32c7a95a5c3c8f4722614abf4cce69354214b081
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-07-06 13:33:28 +0900

    install for axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import router from './router.js'
+import axios from 'axios'
 import SocialSharing from 'vue-social-sharing'
+
+Vue.prototype.$http = axios;
 
 Vue.use(SocialSharing);
 
