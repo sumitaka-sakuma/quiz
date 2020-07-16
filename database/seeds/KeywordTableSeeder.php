73f46fc48fe891b9175934021b6e73bdb25625fe
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-07-16 14:33:14 +0900

    keyword

---
+++ database/seeds/KeywordTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KeywordTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
