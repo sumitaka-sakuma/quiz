0f0c8437ba91858e03ff905bae87a090dfff4962
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-07-16 14:09:08 +0900

    admin menu

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,10 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('home');
 
+    $router->resource('/information', InformationController::class);
+
+    $router->resource('/categories', CategoryController::class);
+
+    $router->resource('/users', UserController::class);
+
 });
