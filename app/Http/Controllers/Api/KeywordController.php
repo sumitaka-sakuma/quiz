63cc1499c71880db085b16b84899cc233da3d61b
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-07-17 11:37:29 +0900

    keyword.vue

---
+++ app/Http/Controllers/Api/KeywordController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Keyword;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class KeywordController extends Controller
+{
+    public function index(Request $request)
+    {
+        $initial = $request->input('initial');
+        $keyword = Keyword::with('category')
+            ->where('keywords.initial', '=',  $initial)
+            ->orderby('keywords.keyword')
+            ->get();
+
+        return $keyword;
+    }
+}
